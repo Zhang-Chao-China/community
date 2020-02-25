e499af48f378897e58714a4c26446aed5e25e667
Zhang-Chao-China <zc1127516754@163.com>
2020-02-25 08:38:36 +0800

    get icon

--- src/main/java/life/majiang/community/community/controller/PublishController.java
+++ src/main/java/life/majiang/community/community/controller/PublishController.java
@@ -21,58 +21,62 @@ public class PublishController {
     private QestionMapper qestionMapper;
     @Autowired
     private UserMapper userMapper;
+
     @GetMapping("/publish")
-    public String publish(){
+    public String publish() {
         return "publish";
     }
+
     @PostMapping("/publish")
     public String doPublish(
-            @RequestParam(value = "title",required=false) String title,
-            @RequestParam(value = "description",required = false) String description,
-            @RequestParam(value = "tag",required = false) String tag,
+            @RequestParam(value = "title", required = false) String title,
+            @RequestParam(value = "description", required = false) String description,
+            @RequestParam(value = "tag", required = false) String tag,
             HttpServletRequest request,
-            Model model){
-        model.addAttribute("title",title);
-        model.addAttribute("description",description);
-        model.addAttribute("tag",tag);
-        if(title==null || title==""){
-            model.addAttribute("error","标题不能为空");
+            Model model) {
+        model.addAttribute("title", title);
+        model.addAttribute("description", description);
+        model.addAttribute("tag", tag);
+        if (title == null || title == "") {
+            model.addAttribute("error", "标题不能为空");
             return "publish";
         }
-        if(description==null || description==""){
-            model.addAttribute("error","问题补充不能为空");
+        if (description == null || description == "") {
+            model.addAttribute("error", "问题补充不能为空");
             return "publish";
         }
-        if(tag==null || tag==""){
-            model.addAttribute("error","标签不能为空");
+        if (tag == null || tag == "") {
+            model.addAttribute("error", "标签不能为空");
             return "publish";
         }
 
-                User user = null;
-                Cookie[] cookies = request.getCookies();
-                for (Cookie cookie : cookies) {
-                    if ("token".equals(cookie.getName())){
-                        String token = cookie.getValue();
-                        user = userMapper.findByToken(token);
-                        if(user != null){
-                            request.getSession().setAttribute("user",user);
-                        }
-                        break;
+        User user = null;
+        Cookie[] cookies = request.getCookies();
+        if (cookies != null && cookies.length != 0) {
+            for (Cookie cookie : cookies) {
+                if ("token".equals(cookie.getName())) {
+                    String token = cookie.getValue();
+                    user = userMapper.findByToken(token);
+                    if (user != null) {
+                        request.getSession().setAttribute("user", user);
                     }
+                    break;
                 }
-                if (user == null){
-                    model.addAttribute("error","用户未登录");
-                    return "publish";
-                }
-                Question question = new Question();
-                question.setTitle(title);
-                question.setDescription(description);
-                question.setTag(tag);
-                question.setCreator(user.getId());
-                question.setGmtCreate(System.currentTimeMillis());
-                question.setGmtModified(question.getGmtCreate());
+            }
+        }
+        if (user == null) {
+            model.addAttribute("error", "用户未登录");
+            return "publish";
+        }
+        Question question = new Question();
+        question.setTitle(title);
+        question.setDescription(description);
+        question.setTag(tag);
+        question.setCreator(user.getId());
+        question.setGmtCreate(System.currentTimeMillis());
+        question.setGmtModified(question.getGmtCreate());
 
-                qestionMapper.create(question);
-                return "redirect:/";
+        qestionMapper.create(question);
+        return "redirect:/";
     }
 }

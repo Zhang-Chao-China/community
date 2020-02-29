4ad664b51d71a647728710f4258f3f788c03a887
Zhang-Chao-China <zc1127516754@163.com>
2020-02-29 13:37:24 +0800

    update

--- src/main/java/life/majiang/community/community/controller/IndexController.java
+++ src/main/java/life/majiang/community/community/controller/IndexController.java
@@ -1,7 +1,6 @@
 package life.majiang.community.community.controller;
 
 import life.majiang.community.community.dto.PaginationDTO;
-import life.majiang.community.community.dto.QuestionDTO;
 import life.majiang.community.community.mapper.UserMapper;
 import life.majiang.community.community.model.User;
 import life.majiang.community.community.service.QuestionService;
